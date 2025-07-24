086eff3cdfd98762241e3e0bd8fab5ae38ce583e
Senthamil-Selvan-17 <senthamil1703@gmail.com>
2025-07-24 20:04:31 +0530

    feat:added new page

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("new_contact", views.new_contact, name="new_contact"),
+    path("show_contact", views.show_contact, name="show_contact")
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 
 def index(request):
     return render(request, "contacts/index.html")
+
+def new_contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        mobile_no = request.POST["mobile_no"]
+        email = request.POST["email"]
+
+        Contact.objects.create(name=name, mobile_no=mobile_no, email=email)
+        return render(request, "contacts/new_contact.html", {
+            "saved": "Contact saved"
+        })
+
+    return render(request, "contacts/new_contact.html")
+
+def show_contact(request):
+    all_contacts = Contact.objects.all()
+    return render(request, "contacts/show_contact.html", {
+        "all_contacts": all_contacts
+    })
